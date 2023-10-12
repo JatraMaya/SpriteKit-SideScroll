dd2e9b23f96a22fdae73c8703e7978c691e2130e
Ahmad F. Iksan <ahmad.f.iksan@gmail.com>
2023-10-12 22:08:33 +0700

    Add Control class to display buttons

--- Gaming/NodeClass/Control.swift
+++ Gaming/NodeClass/Control.swift
@@ -6,3 +6,36 @@
 //
 
 import Foundation
+import SpriteKit
+
+class Control {
+
+    enum actionState {
+        case isAction
+        case isTalking
+    }
+
+    var buttonLeft = SKLabelNode(text: "◀️")
+    var buttonRight = SKLabelNode(text: "▶️")
+    var buttonAction = SKLabelNode(text: "🤚")
+
+    init(buttonLeft: SKLabelNode = SKLabelNode(text: "◀️"), buttonRight: SKLabelNode = SKLabelNode(text: "▶️"), buttonAction: SKLabelNode = SKLabelNode(text: "🤚")) {
+        self.buttonLeft = buttonLeft
+        self.buttonRight = buttonRight
+        self.buttonAction = buttonAction
+
+        self.buttonLeft.name = "buttonLeft"
+        self.buttonRight.name = "buttonRight"
+        self.buttonAction.name = "buttonAction"
+    }
+
+    func updateButtonState(state: actionState){
+        let state = state
+        switch state {
+        case .isAction:
+            buttonAction.text = "🤚"
+        case .isTalking:
+            buttonAction.text = "👄"
+        }
+    }
+}
