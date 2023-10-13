c78e7801fb1694cfc9d7d60b627aca1a5b695ac8
Ahmad F. Iksan <ahmad.f.iksan@gmail.com>
2023-10-13 08:50:06 +0700

    Add function description

--- Gaming/NodeClass/Player.swift
+++ Gaming/NodeClass/Player.swift
@@ -13,6 +13,8 @@ class Player: SKSpriteNode {
     let frame1 = SKTexture(imageNamed: "player1")
     let frame2 = SKTexture(imageNamed: "player2")
 
+    let playerCategory: UInt32 = 1 >> 1
+
     var playerMoveLeft = false
     var playerMoveRight = false
 
@@ -21,16 +23,19 @@ class Player: SKSpriteNode {
         self.name = "player"
         self.physicsBody = SKPhysicsBody(texture: frame1, size: frame1.size())
         self.physicsBody?.affectedByGravity = false
+        self.physicsBody?.categoryBitMask = playerCategory
     }
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
     }
-
+    
+    /// Handling Player walking animation with SKAction node
     func walkingAnimation() {
         let animate = SKAction.animate(with: [frame1, frame2], timePerFrame: 0.10)
         self.run(SKAction.repeatForever(animate))
     }
-
+    
+    /// Update player location in the screen based on delta time and condition either playerMoveLeft/playerMoveRight boolean value
     func updatePlayerPosition() {
         if playerMoveLeft {
             self.xScale = 1
@@ -40,7 +45,8 @@ class Player: SKSpriteNode {
             self.position.x += 1
         }
     }
-
+    
+    /// Set value for both playerMoveLeft & playerMoveRight back to false and remove all SKAction triggered by previous conditional
     func stopPlayerMovement() {
         playerMoveLeft = false
         playerMoveRight = false
