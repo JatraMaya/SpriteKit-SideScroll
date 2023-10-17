8cf377988dd9ffa6bd8d45b45a08710774c6d010
Ahmad F. Iksan <ahmad.f.iksan@gmail.com>
2023-10-17 23:44:38 +0700

    xcodeproj stuff

--- Gaming/GameScene.swift
+++ Gaming/GameScene.swift
@@ -13,14 +13,17 @@ class GameScene: SKScene {
     var dialogIsActive = false
     var activeNpc = ""
 
-    var player: Player
-    var control: Control
-    var npc1: Npc
-    var npc2: Npc
+
+    let player: Player
+    let cameraNode: SKCameraNode
+    let npc1: Npc
+    let npc2: Npc
+    let shape = SKSpriteNode(color: UIColor.blue, size: CGSize(width: 10, height: 10))
 
     override init(size: CGSize) {
+
+        cameraNode = SKCameraNode()
         player = Player()
-        control = Control()
         npc1 = Npc(size: size, imageName: "npc-b-1", imageNpc: "npc-b-1", npcName: "npc1")
         npc2 = Npc(size: size, imageName: "npc-a-1", imageNpc: "npc-a-1", npcName: "npc2")
         super.init(size: size)
@@ -34,8 +37,9 @@ class GameScene: SKScene {
     override func didMove(to view: SKView) {
 
         setupPlayer()
-        setupControl()
+        setupCamera()
         setupNpc()
+
 
         scene!.name = "scene"
 
@@ -44,89 +48,51 @@ class GameScene: SKScene {
     // Update Scene (including node location) accroding to delta time
     override func update(_ currentTime: TimeInterval) {
         player.updatePlayerPosition()
-        updateActionButtonText()
+        updateActionSpeechMark()
+        camera?.position.x = player.position.x
     }
 
     // control functionality when button is touch
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        setupControlTouches(touches)
+        for touch in (touches) {
+            let location = touch.location(in: self.camera!)
+            print(location.x)
+            if location.x < 1 {
+                player.playerMoveLeft = true
+                player.playerMoveRight = false
+                player.walkingAnimation()
+            }else {
+                player.playerMoveRight = true
+                player.playerMoveLeft = false
+                player.walkingAnimation()
+            }
+        }
     }
 
     // Tracking to stop functionality when button is no longer pressed
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
         player.stopPlayerMovement()
-        control.resetButtonAlpha()
     }
 
     // MARK: Various setup functions needed to build a scene
     /// Function to setup player to the scene,
     func setupPlayer() {
+        let questionMark = SKLabelNode(text: "💬")
+        questionMark.alpha = 0
+        questionMark.fontSize = 20
+        questionMark.name = "speechBubble"
         player.anchorPoint = CGPoint(x: 0.5, y: 0.5)
         player.position = CGPoint(x: frame.minX + 80, y: frame.minY + 120)
         addChild(player)
-    }
-    
-    /// Function to setup three buttons control on the scene
-    func setupControl() {
-        control.buttonLeft.position = CGPoint(x: frame.minX + 45, y: frame.minY + 50)
-        control.buttonRight.position = CGPoint(x: frame.minX + 100, y: frame.minY + 50)
-        control.buttonAction.position = CGPoint(x: frame.maxX - 45, y: frame.minY + 50)
-        addChild(control.buttonLeft)
-        addChild(control.buttonRight)
-        addChild(control.buttonAction)
-    }
-    
-    /// Function to handle player touches to the controller node
-    /// - Parameter touches: require Set of UITouch
-    func setupControlTouches(_ touches: Set<UITouch>) {
-        for touch in touches {
-            let location = touch.location(in: self)
-            let node = self.atPoint(location)
+        player.addChild(questionMark)
+        questionMark.position.y = player.position.x - 65
 
-            if node.name == "buttonLeft" {
-                player.playerMoveLeft = true
-                player.playerMoveRight = false
-                control.updateButtonAlpha("buttonLeft")
-                player.walkingAnimation()
-            } else if node.name == "buttonRight" {
-                player.playerMoveLeft = false
-                player.playerMoveRight = true
-                control.updateButtonAlpha("buttonRight")
-                player.walkingAnimation()
-            } else if node.name == "buttonAction" {
-                if control.buttonAction.text == "👄" {
-                    if scene?.childNode(withName: "dialogBox") == nil {
-                        if activeNpc == "npc1" {
-                            addChild(npc1.dialogBox)
-                            npc1.setupDialog()
-                        } else if activeNpc == "npc2" {
-                            addChild(npc2.dialogBox)
-                            npc2.setupDialog()
-                        }
-                    }
-                }
-            }
-
-            if node.name == "dialogBox" {
-                if activeNpc == "npc1" {
-                    npc1.updateDialog()
-                } else if activeNpc == "npc2" {
-                    npc2.updateDialog()
-                }
-            }
-        }
     }
 
-    func updateActionButtonText() {
-            if (-40..<35).contains(player.position.x - npc1.sprite.position.x) {
-                control.updateButtonState(state: .isTalking)
-                activeNpc = "npc1"
-            } else if (-40..<35).contains(player.position.x - npc2.sprite.position.x) {
-                control.updateButtonState(state: .isTalking)
-                activeNpc = "npc2"
-            } else {
-                control.updateButtonState(state: .isAction)
-            }
+    func setupCamera() {
+        cameraNode.position = CGPoint(x: size.width / 2, y: size.height / 2)
+        addChild(cameraNode)
+        camera = cameraNode
     }
 
     func setupNpc() {
@@ -138,4 +104,20 @@ class GameScene: SKScene {
             i.dialogBox.position = CGPoint(x: frame.minX + 400, y: frame.minY + 100)
         }
     }
+
+    func updateActionSpeechMark() {
+                if (-40..<35).contains(player.position.x - npc1.sprite.position.x) {
+                    player.childNode(withName: "speechBubble")?.alpha = 1
+                    activeNpc = "npc1"
+                } else if (-40..<35).contains(player.position.x - npc2.sprite.position.x) {
+                    player.childNode(withName: "speechBubble")?.alpha = 1
+                    activeNpc = "npc2"
+                } else {
+                    player.childNode(withName: "speechBubble")?.alpha = 0
+                }
+        }
+
+//    func setupScreenTap() {
+//        let leftScreenBoundries =
+//    }
 }
